cc4ebbe3b3ae920c38d1ac4e31182f317e8e1039
radioactive11 <roy.arijit2001@gmail.com>
2021-05-22 17:12:11 +0530

    implemented a callback

--- public/index.js
+++ public/index.js
@@ -2,9 +2,16 @@ const sio = io();
 
 sio.on('connect', ()=>{
     console.log("Connected");
+    sio.emit('sum', {numbers: [1, 2]}, (result)=>{
+        console.log(result);
+    });
 });
 
 sio.on('disconnect', ()=>{
     console.log("Disconnected");
 });
 
+sio.on('mult', (data, cb)=>{
+    const result = data.numbers[0] * data.numbers[1];
+    cb(result);
+})
