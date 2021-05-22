17ebc9786d26e8845b7db6e277c97c96454e5549
radioactive11 <roy.arijit2001@gmail.com>
2021-05-22 17:11:54 +0530

    added callback to server

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app = socketio.WSGIApp(sio, static_files={
 })
 
 
+def task(sid):
+    sio.sleep(3)
+    result = sio.call('mult', {'numbers': [3, 4]}, to=sid)
+    print(result)
+
+
+
 @sio.event
 def connect(sid, environ):
     """
@@ -14,12 +21,19 @@ def connect(sid, environ):
     environ is a dictionary which has all the details from the client request
     """
     print(f"[CONNECTED] {sid}")
-    
-
-    
+    sio.start_background_task(task, sid)
 
 @sio.event
 def disconnect(sid):
     print(f"[DISCONNECTED] {sid}")
+
+
+@sio.event
+def sum(sid, data):
+    result = data['numbers'][0] + data['numbers'][1]
+    # sio.emit('sum_result', {'result': result}, to=sid)
+    return {'result': result}
+
+
 
 # Use a WSGI webserver like gunicron to run the server
